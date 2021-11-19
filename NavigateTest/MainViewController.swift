d533852be0eaf9e1131f8b4a2299fbda1971507a
tareq3 <mti.tareq3@gmail.com>
2021-11-19 23:46:56 +0600

    udpated navigation a bit

--- NavigateTest/MainViewController.swift
+++ NavigateTest/MainViewController.swift
@@ -10,26 +10,38 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBAction func nextButton(_ sender: Any) {
-        navigateUsingNavigation()
+        // navigateUsingPresentFromAnyStoryBoard()
+        //navigateUsingNavigation()
+        basicNav()
     }
+    //for showing dialog
+    func navigateUsingPresentFromAnyStoryBoard(){
+            let story = UIStoryboard(name: "Alert", bundle: Bundle.main)
+            let secondVC = story.instantiateViewController(identifier: "second_vc") as! SecondViewController
+            secondVC.modalPresentationStyle = .fullScreen //for full screen mode like navigation else it would look like modal sheet
+            secondVC.modalTransitionStyle = .coverVertical
+            self.present(secondVC, animated: true, completion:  nil)
+        }
+
     
-   func navigateUsingPresentFromAnyStoryBoard(){
-        let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        let secondVC = storyboard?.instantiateViewController(identifier: "second_vc") as! SecondViewController
-        secondVC.modalPresentationStyle = .fullScreen //for full screen mode like navigation else it would look like modal sheet
-        secondVC.modalTransitionStyle = .coverVertical
-        self.present(secondVC, animated: true, completion:  nil)
-    }
+    ///navigation is like fragment's replacing layout while parent activity will be alawys active
     
     func navigateUsingNavigation(){
-        let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        let secondVC = storyboard?.instantiateViewController(identifier: "second_vc") as! SecondViewController
+        let story = UIStoryboard(name: "Alert", bundle: nil)
+        let secondVC = story.instantiateViewController(identifier: "second_vc") as! SecondViewController
        
         let navigation = UINavigationController(rootViewController: secondVC)
         self.view.addSubview(navigation.view)
         self.addChild(navigation)
         navigation.didMove(toParent: self)
         
+    }
+    //for normal activity transaction
+    func basicNav(){
+         let story = UIStoryboard(name: "Alert", bundle: nil)
+                    let vc = story.instantiateViewController(withIdentifier: "second_vc") as! SecondViewController
+                   
+                    self.navigationController?.pushViewController(vc, animated: true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
